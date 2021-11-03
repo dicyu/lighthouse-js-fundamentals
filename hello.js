e2b4f31a055f790972f0206dc1b2e866e193136c
Dickson Yu <dickson.yu@email.com>
2021-11-03 16:08:32 -0400

    added the use of return

--- hello.js
+++ hello.js
@@ -7,3 +7,15 @@ const sayHello = function(name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdiand");
+
+// code 2
+const sayHelloToConsole = function(name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
