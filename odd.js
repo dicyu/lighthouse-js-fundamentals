f5e61348b2a3a1a341a6996880339f671a6c202b
Dickson Yu <dickson.yu@email.com>
2021-11-03 16:35:19 -0400

    odd checker challenge

---
+++ odd.js
@@ -0,0 +1,11 @@
+// global variables
+
+// code
+const isOdd = function(num) { // function for declaring number of odd or even
+  return (num % 2 === 1);
+}
+ // output
+console.log("3 is odd: " + isOdd(3));
+console.log("2 is odd: " + isOdd(2));
+console.log("133 is odd: " + isOdd(133));
+console.log("122 is odd: " + isOdd(122));
