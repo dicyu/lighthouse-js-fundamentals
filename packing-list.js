4e563f9fcc49e36003cdcaeb1438311bf8e984b1
Dickson Yu <dickson.yu@email.com>
2021-11-04 13:48:16 -0400

    kitchen packing list

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+// global variables
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+// code
+console.log("Kitchen stuff to pack");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
