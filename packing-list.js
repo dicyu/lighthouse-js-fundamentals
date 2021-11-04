7300e352e89066e9e5a68d89b3c323ffcb48e767
Dickson Yu <dickson.yu@email.com>
2021-11-04 13:50:53 -0400

    added a while loop

--- packing-list.js
+++ packing-list.js
@@ -6,3 +6,11 @@ console.log("Kitchen stuff to pack");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+// while loop
+console.log("Kitchen stuff to pack");
+let x = 0;
+while (x < packingList.length) {
+  console.log(packingList[x]);
+  x++;
+}
